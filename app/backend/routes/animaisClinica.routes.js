a406df1d298c1ea8e1bb87ec0eb438aec3d74808
Erick Tavares <ericktnunes.2004@gmail.com>
2025-11-24 22:58:14 -0300

    add rotas a formulario e atualizando rotas get

--- app/backend/routes/animaisClinica.routes.js
+++ app/backend/routes/animaisClinica.routes.js
@@ -13,9 +13,21 @@ router.post('/', create);
 router.put('/:id', update);
 router.delete('/:id', remove);
 <<<<<<< HEAD
+<<<<<<< HEAD
 // router.get('/', getAllEvents)
 =======
 //router.get('/', getAllEvents)
 >>>>>>> c439bf9 (add rotas ao formulário de adição de animais para adoção e correções de rotas GET)
+=======
+<<<<<<< Updated upstream
+//router.get('/', getAllEvents)
+=======
+<<<<<<< HEAD
+// router.get('/', getAllEvents)
+=======
+//router.get('/', getAllEvents)
+>>>>>>> c439bf9 (add rotas ao formulário de adição de animais para adoção e correções de rotas GET)
+>>>>>>> Stashed changes
+>>>>>>> 46734a9 (Resolve conflitos após stash)
 
 export default router;
